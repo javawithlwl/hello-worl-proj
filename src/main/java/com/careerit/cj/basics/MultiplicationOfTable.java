e3392d1a82edf2911074b84cdf45f98ce7eb2850
javawithlwl <javawithlwl@gmail.com>
2024-03-19 06:50:22 +0530

    Added hello world project

---
+++ src/main/java/com/careerit/cj/basics/MultiplicationOfTable.java
@@ -0,0 +1,11 @@
+package com.careerit.cj.basics;
+
+public class MultiplicationOfTable {
+
+    public static void main(String[] args) {
+        int num = 19;
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(num + " * " + i + " = " + (num * i));
+        }
+    }
+}
